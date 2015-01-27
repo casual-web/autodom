705cf63e2b0e4a32bfac1a6e211394f0c205b229
olivier <olivierlequeux@yahoo.fr>
2015-01-27 22:53:39 +0100

    adding entity BusinessService and related fixtures

--- src/AppBundle/DataFixtures/ORM/LoadBusinessServicesData.php
+++ src/AppBundle/DataFixtures/ORM/LoadBusinessServicesData.php
@@ -20,32 +20,33 @@ class LoadBusinessServicesData implements FixtureInterface {
      */
     public function load(ObjectManager $manager)
     {
-        $userAdmin = new BusinessService();
-        $userAdmin->setName('Débosselage sans peinture');
-        $userAdmin->setRef('DSP');
-        $userAdmin->setDescription("Le débosselage sans peinture est un procédé qui permet de résorber des bosses sur la carrosserie des véhicules automobiles.");
-        $manager->persist($userAdmin);
-        $manager->flush();
+        $bsDSP = new BusinessService();
+        $bsDSP->setName('Débosselage sans peinture');
+        $bsDSP->setRef('DSP');
+        $bsDSP->setDescription("Le débosselage sans peinture est un procédé qui permet de résorber des bosses sur la carrosserie des véhicules automobiles.");
+        $manager->persist($bsDSP);
 
-        $userAdmin = new BusinessService();
-        $userAdmin->setName('Rénovation optiques');
-        $userAdmin->setRef('OPT');
-        $userAdmin->setDescription("Sur la base d'un ponçage du phare opacifié, deux processus sont proposés afin de retrouver l'éclat d'origine d'un phare terni : le Polish et le Revernis.");
-        $manager->persist($userAdmin);
-        $manager->flush();
 
-        $userAdmin = new BusinessService();
-        $userAdmin->setName("Rénovation carrosserie");
-        $userAdmin->setRef('CAR');
-        $userAdmin->setDescription("Sur la base d'un ponçage du phare opacifié, deux processus sont proposés afin de retrouver l'éclat d'origine d'un phare terni");
-        $manager->persist($userAdmin);
-        $manager->flush();
+        $bsOPT = new BusinessService();
+        $bsOPT->setName('Rénovation optiques');
+        $bsOPT->setRef('OPT');
+        $bsOPT->setDescription("Sur la base d'un ponçage du phare opacifié, deux processus sont proposés afin de retrouver l'éclat d'origine d'un phare terni : le Polish et le Revernis.");
+        $manager->persist($bsOPT);
 
-        $userAdmin = new BusinessService();
-        $userAdmin->setName('Remplacement vitrage');
-        $userAdmin->setRef('VIT');
-        $userAdmin->setDescription("Remplacement de tout vitrage sur véhicule léger dans le respect des normes en vigueur de la communauté européenne. Si vous êtes assuré 'bris de glace' : nous vérifions ensemble que votre assurance prend bien le sinistre en charge.");
-        $manager->persist($userAdmin);
+
+        $bsCAR = new BusinessService();
+        $bsCAR->setName("Rénovation carrosserie");
+        $bsCAR->setRef('CAR');
+        $bsCAR->setDescription("Sur la base d'un ponçage du phare opacifié, deux processus sont proposés afin de retrouver l'éclat d'origine d'un phare terni");
+        $manager->persist($bsCAR);
+
+
+        $bsVIT = new BusinessService();
+        $bsVIT->setName('Remplacement vitrage');
+        $bsVIT->setRef('VIT');
+        $bsVIT->setDescription("Remplacement de tout vitrage sur véhicule léger dans le respect des normes en vigueur de la communauté européenne. Si vous êtes assuré 'bris de glace' : nous vérifions ensemble que votre assurance prend bien le sinistre en charge.");
+        $manager->persist($bsVIT);
+
         $manager->flush();
 
     }
