e5a1dd9f738f127c66a9f14c2964cd48b012e09e
olivier <olivierlequeux@yahoo.fr>
2015-02-08 19:46:16 +0100

    adding basic navbar

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -3,19 +3,25 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * BusinessService controller.
+ *
+ * @Route("/admin/tableaudebord")
+ */
 class AdminController extends Controller
 {
     /**
-     * @Route("/admin")
+     * @Route("/", name="admin_tableaudebord")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction()
     {
-        return array(// ...
-        );
+        return array();
     }
 
 }
