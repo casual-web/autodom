262c47389671b39af9669fbef4c0f2d100ff812e
casual-web <olivier.lequeux@attestationlegale.fr>
2017-02-25 14:05:31 +0100

    fix buf submmit form with no ref + wording

--- src/AppBundle/Tests/Entity/QuotationRequestServiceRelationCollectionTest.php
+++ src/AppBundle/Tests/Entity/QuotationRequestServiceRelationCollectionTest.php
@@ -24,4 +24,10 @@ class QuotationRequestServiceRelationCollection extends \PHPUnit_Framework_TestC
         $this->assertEquals(['DSP', 'CAR'], $references->getBusinessServiceReferences());
     }
 
+    public function testNoGetReferences()
+    {
+
+        $references = new \AppBundle\Entity\QuotationRequestServiceRelationCollection([]);
+        $this->assertEquals([], $references->getBusinessServiceReferences());
+    }
 }
